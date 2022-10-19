99fedc23ae6b432834b3f71c79e0103895059f31
kebuk02 <sayadepan020702@gmail.com>
2022-10-19 20:46:04 +0700

    upoad tugas web

---
+++ keuangan/koneksi.php
@@ -0,0 +1,22 @@
+<?php
+$koneksi = mysqli_connect("localhost","root","","keuangan");
+
+// Check connection
+if (mysqli_connect_errno())
+    echo "koneksi database gagal : " . mysqli_connect_errno();
+else
+    echo "alhamdulillah bisa ya allah";
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
